355a3ca3fc0f61806779ef7c1ebffd1b104ba520
Petra <petra@behemot.biz>
2024-11-21 10:35:55 +0100

    feat: Add measurement and ingredient models and intermediate model RecipeIngredient to recipes app

--- recipes/models.py
+++ recipes/models.py
@@ -2,6 +2,26 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Measurement(models.Model):
+    """
+    Measurement model for storing unit types (e.g., grams, cups, teaspoons).
+    """
+    measure = models.CharField(max_length=50, unique=True)  # e.g., "g", "cup", "ml"
+
+    def __str__(self):
+        return self.measure
+
+
+class Ingredient(models.Model):
+    """
+    Ingredient model for storing common ingredients (e.g., water, sugar, flour).
+    """
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Recipe(models.Model):
     """
     Recipe model, related to 'owner', i.e. a User instance.
@@ -12,7 +32,6 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     recipe_name = models.CharField(max_length=255)
     intro = models.TextField(blank=True)
-    ingredients = models.TextField(blank=True)
     instruction = models.TextField(blank=True)
     image = models.ImageField(
         upload_to='images/', default='../default_profile_lpzfbh', blank=True
@@ -23,3 +42,17 @@ class Recipe(models.Model):
 
     def __str__(self):
         return f'{self.id} {self.recipe_name}'
+
+
+class RecipeIngredient(models.Model):
+    """
+    Intermediate model to store the relationship between Recipe and Ingredient,
+    including the quantity and measurement.
+    """
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='recipe_ingredients')
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='ingredient_recipes')
+    quantity = models.CharField(max_length=50)
+    measure = models.ForeignKey(Measurement, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.quantity} {self.measure} of {self.ingredient} for {self.recipe}'
