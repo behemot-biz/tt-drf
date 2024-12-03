d5793763551b707ece3a23be5f962ece3dee80c4
Petra <petra@behemot.biz>
2024-12-03 10:38:13 +0100

    feat: Update profiles with filters, recipes count, follower and following counts

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,18 +1,58 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Profile model.
+
+    This serializer provides:
+        - Basic profile details such as `name`, `content`, and `image`.
+        - Ownership information (`is_owner`) to determine if the current user
+            owns the profile.
+        - Follow status (`following_id`), which indicates if the current user
+            is following this profile.
+        - Aggregated counts for:
+            - `recipes_count`: The number of recipes created by profile owner.
+            - `followers_count`: The number of users following profile owner.
+            - `following_count`: The number of users  profile owner follows.
+
+    Methods:
+        - `get_is_owner`:
+            Determines if profile belongs to the currently authenticated user.
+        - `get_following_id`:
+            Retrieves the ID of the `Follower` relationship if the current
+            user follows the profile owner.
+
+    Attributes:
+        - `model`: Specifies the Profile model being serialized.
+        - `fields`: Defines fields exposed in the serialized representation.
+    """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
+    recipes_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            return following.id if following else None
+        return None
+
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'name', 
-            'content', 'image', 'is_owner'
+            'id', 'owner', 'created_at', 'updated_at', 'name',
+            'content', 'image', 'is_owner', 'following_id',
+            'recipes_count', 'followers_count', 'following_count',
         ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,19 +1,41 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from tt_drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
 
 
 class ProfileList(generics.ListAPIView):
+    """
+    API view to retrieve a list of profiles.
+    """
+    queryset = Profile.objects.annotate(
+        recipes_count=Count('owner__recipe', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
-    
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'recipes_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+    ]
+
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
-    serializer_class = ProfileSerializer
+    """
+    API view to retrieve or update a specific profile.
+    """
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
-   
-
-
-    
+    # queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        recipes_count=Count('owner__recipe', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
+    serializer_class = ProfileSerializer
